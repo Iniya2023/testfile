ebae4e2b3fc799856543e98fb161720bb0f573c1
Iniya2023 <mohanaraji99@gmail.com>
2025-06-19 12:36:30 +0530

    uptated test file

--- main.js
+++ main.js
@@ -1,34 +1,36 @@
-/*let c=0;
+let c=0;
 let a ,r;
 a=parseInt(prompt("Enter the number"));
 let temp=a;
+let digit = a.toString().length;
 while(a!=0){
 	r=a%10;
-	c+=r*r*r;
+	c+=Math.pow(r,digit);
 	a=parseInt(a/10);	
 }
 if(c==temp){
 	
 	
-	console.log("This Number is Amstrong Number");
+	console.log(temp + "This Number is Amstrong Number");
 }
 else{
 	
-	console.log("This Number is Not a Amstrong Number");
+	console.log(temp + "This Number is Not a Amstrong Number");
 	
 }
-*/
 
-/*let amstrong = [1, 245, 153, 370, 456, 123, 270];
+
+/*let amstrong = [1, 245, 153, 370, 456, 123, 270,3,8,9,7];
 
 for (let i = 0; i < amstrong.length; i++) {
     let n = amstrong[i];
     let temp = n;
     let c = 0;
+	let digit = n.toString().length;
 
-    while (n != 0) {
+    while (n !==0) {
         let r = n % 10;
-        c += r * r * r;
+        c += Math.pow(r,digit);
         n = parseInt(n / 10);
     }
 
@@ -36,16 +38,18 @@ for (let i = 0; i < amstrong.length; i++) {
         console.log(temp + " is an Armstrong number");
     }
 }
-*/
+
+
 let amstrong = [];
 for (let i = 1; i < 1000; i++) {
     let n = i;
     let temp = n;
     let c = 0;
+	let digit = n.toString().length;
 
     while (n != 0) {
         let r = n % 10;
-        c += r * r * r;
+       c += Math.pow(r,digit);
         n = parseInt(n / 10);
     }
 
@@ -56,5 +60,4 @@ for (let i = 1; i < 1000; i++) {
 }
 console.log("Amstrong Numbers are 1-999");
 console.log(amstrong);
-
-
+*/
